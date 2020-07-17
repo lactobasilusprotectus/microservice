323eeb267c832ea545bd11b669b149809b76e7f9
Amru Fakharullah <amru1731218@sttpln.ac.id>
2020-07-17 20:44:17 +0700

    integarted service-media to api-gateway and add service-users

--- service-user/app.js
+++ service-user/app.js
@@ -6,24 +6,16 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const coursesRouter = require('./routes/courses');
-const mediaRouter = require('./routes/media');
-const ordersRouter = require('./routes/orders');
-const paymentsRouter = require('./routes/payments');
 
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/courses', coursesRouter);
-app.use('/media', mediaRouter);
-app.use('/orders', ordersRouter);
-app.use('/payments', paymentsRouter);
 
 module.exports = app;
